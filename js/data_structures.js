2a8f31a6af8cf8287a95a347ab664106674fbda5
April Tang <april.tang09@gmail.com>
2016-09-18 18:26:08 -0700

    Add Car objects

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,24 @@ for (var i = 0; i < names.length; i++){
 }
 
 console.log(horses);
+
+function Car(color, isManuel, year){
+
+	console.log("Our new car:", this);
+	this.color = color;
+	this.isManuel = isManuel;
+	this.year = year;
+
+	this.honk = function(){console.log("Honk!");};
+
+	console.log("CAR INITIALIZATION COMPLETE");
+}
+
+var usedCar = new Car("green", true, 1999);
+console.log(usedCar);
+usedCar.honk();
+
+var newCar = new Car("white", false, 2016);
+console.log(newCar);
+newCar.honk();
+
