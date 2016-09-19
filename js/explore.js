2deb6ba4004c8348fdaa3c86a32b19df9e72e6c1
April Tang <april.tang09@gmail.com>
2016-09-19 09:39:36 -0700

    Add better method that uses for loop

--- js/explore.js
+++ js/explore.js
@@ -1,13 +1,20 @@
+// loop through the word starting at the end
+// for each looped letter, add it to a variable that holds all the 
+// letters together
+
 function reverse(string){
-	return string.split("").reverse().join("");
+	
+	var word = "";
+	
+	for(var i = string.length -1 ; i >= 0; i--){
+		word += string[i];
+	}
+	return word;
 }
 
-var morning = reverse("hello");
-console.log(morning);
-var evening = reverse("goodbye");
-console.log(evening);
+var greeting = reverse("hello");
 
-if (2 != 3){
-	console.log(morning);
-	console.log(evening);
+
+if (5 != 4){
+	console.log(greeting);
 }
