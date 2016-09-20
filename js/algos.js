3e07fcbd01ac2bb12f695280b4f5b3d99705e465
April Tang <april.tang09@gmail.com>
2016-09-20 07:02:47 -0700

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,4 @@
+// loop through each phase
+// compare the phrase length to the one after it
+// if that phase length is longer, hold that in a variable 
+// have that variable compare to the next phrase length
