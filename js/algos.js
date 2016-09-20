c5ab76da1941260a765d977ac66dd6758683c66e
April Tang <april.tang09@gmail.com>
2016-09-20 07:04:12 -0700

    Create function that returns longest phrase

--- js/algos.js
+++ js/algos.js
@@ -2,3 +2,17 @@
 // compare the phrase length to the one after it
 // if that phase length is longer, hold that in a variable 
 // have that variable compare to the next phrase length
+
+function algos(array){	
+	var longest = 0
+	
+	for (var i = 0; i < array.length; i++){	
+		if(array[i].length > longest){
+			longest = array[i].length
+			var word = array[i]
+		}
+	}	
+console.log(word)	
+}
+
+algos(["long phrase","longest phrase","longer phrase"])
