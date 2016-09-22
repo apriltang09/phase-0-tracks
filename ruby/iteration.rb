6da1f8785ff8ada1b5cc554334ecc0902af21c35
April Tang <april.tang09@gmail.com>
2016-09-21 18:23:14 -0700

    Add .each method on hash, show .map! doesn't work on hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -14,17 +14,18 @@ say_goodbye { |name| puts "Talk to you soon, #{name}!" }
 # This is printing from the method again.
 
 #RELEASE 1
+#ARRAY
 #.EACH doesn't change the original array
 dogs = ["black spaniel", "white poodle", "golden retriever"]
+
+dogs.each do |dog|
+	dog.capitalize
+end
 
 puts "Original dog array:"
 p dogs
 # Original dog array:
 # ["black spaniel", "white poodle", "golden retriever"]
-
-dogs.each do |dog|
-	dog.capitalize
-end
 
 puts "After .each used:"
 p dogs
@@ -32,19 +33,64 @@ p dogs
 # ["black spaniel", "white poodle", "golden retriever"]
 
 #.MAP! changes the original array
+dogs.map! do |dog|
+	dog.upcase
+end
+
 puts "Original dog array:"
 p dogs
 # Original dog array:
 # ["black spaniel", "white poodle", "golden retriever"]
 
-dogs.map! do |dog|
-	dog.upcase
-end
-
 puts "After .map! used:"
 p dogs
 # After .map! used:
 # ["BLACK SPANIEL", "WHITE POODLE", "GOLDEN RETRIEVER"]
+
+#HASH
+
+cats = {
+	"bobbie" => "brown",
+	"angelica" => "white",
+	"tabitha" => "spotted",
+	"samantha" => "red"
+}
+
+#.EACH doesn't change the original array
+cats.each do |cat, color|
+	cat.capitalize
+end
+
+puts "Original cat hash: "
+p cats
+# Original cat hash: 
+# {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
+
+puts "After .each used:"
+p cats
+# After .each used:
+# {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
+
+#.MAP! can't be used on hashes
+cats.map! do |cat, color|
+	cat.upcase
+end
+
+puts "Original cat hash: "
+p cats
+# Original cat hash: 
+# {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
+
+
+puts "After .map! used:"
+p cats
+# undefined method `map!' for #<Hash:0x005617ca0bf9e0>
+# Did you mean?  map
+# (repl):44:in `<main>'
+
+
+
+
 
 
 
