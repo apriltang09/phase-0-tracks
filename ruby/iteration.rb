641ae8e075a86eedc252bf3bb89fb2311f4c4c9c
April Tang <april.tang09@gmail.com>
2016-09-21 18:29:57 -0700

    Reorganize code

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,7 +6,7 @@ def say_goodbye
 	puts "This is printing from the method again."
 end
 
-say_goodbye { |name| puts "Talk to you soon, #{name}!" }
+say_goodbye {|name| puts "Talk to you soon, #{name}!"}
 
 # This is printing from the method.
 # Talk to you soon, Angela!
@@ -16,16 +16,19 @@ say_goodbye { |name| puts "Talk to you soon, #{name}!" }
 #RELEASE 1
 #ARRAY
 #.EACH doesn't change the original array
-dogs = ["black spaniel", "white poodle", "golden retriever"]
+#.MAP! changes the original array
 
-dogs.each do |dog|
-	dog.capitalize
-end
+#.EACH doesn't change the original array
+dogs = ["black spaniel", "white poodle", "golden retriever"]
 
 puts "Original dog array:"
 p dogs
 # Original dog array:
 # ["black spaniel", "white poodle", "golden retriever"]
+
+dogs.each do |dog|
+	dog.capitalize
+end
 
 puts "After .each used:"
 p dogs
@@ -33,14 +36,14 @@ p dogs
 # ["black spaniel", "white poodle", "golden retriever"]
 
 #.MAP! changes the original array
-dogs.map! do |dog|
-	dog.upcase
-end
-
 puts "Original dog array:"
 p dogs
 # Original dog array:
 # ["black spaniel", "white poodle", "golden retriever"]
+
+dogs.map! do |dog|
+	dog.upcase
+end
 
 puts "After .map! used:"
 p dogs
@@ -48,6 +51,8 @@ p dogs
 # ["BLACK SPANIEL", "WHITE POODLE", "GOLDEN RETRIEVER"]
 
 #HASH
+#.EACH doesn't change the original array
+#.MAP! can't be used on hashes
 
 cats = {
 	"bobbie" => "brown",
@@ -57,14 +62,14 @@ cats = {
 }
 
 #.EACH doesn't change the original array
-cats.each do |cat, color|
-	cat.capitalize
-end
-
 puts "Original cat hash: "
 p cats
 # Original cat hash: 
 # {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
+
+cats.each do |cat, color|
+	cat.capitalize
+end
 
 puts "After .each used:"
 p cats
@@ -72,22 +77,20 @@ p cats
 # {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
 
 #.MAP! can't be used on hashes
-cats.map! do |cat, color|
-	cat.upcase
-end
-
 puts "Original cat hash: "
 p cats
 # Original cat hash: 
 # {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
 
+cats.map! do |cat, color|
+	cat.upcase
+end
 
 puts "After .map! used:"
 p cats
 # undefined method `map!' for #<Hash:0x005617ca0bf9e0>
 # Did you mean?  map
 # (repl):44:in `<main>'
-
 
 
 
