d6c8959b1e390326aef04a08242f2300761caa6a
April Tang <april.tang09@gmail.com>
2016-09-21 20:25:23 -0700

    Add methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -116,5 +116,80 @@ p update
 # {"bobbie"=>"brown", "angelica"=>"white", "tabitha"=>"spotted", "samantha"=>"red"}
 # ["BOBBIE", "ANGELICA", "TABITHA", "SAMANTHA"]
 
+#RELEASE 2
 
+#1. A method that iterates through the items, deleting any that meet a certain condition
+#ARRAY
+miles = [1, 3, 5, 7, 9]
+miles.delete_if {|mile| mile > 5}
+# => [1, 3, 5]
+p miles
+# [1, 3, 5]
+# => [1, 3, 5]
+
+#HASH
+run = {
+	'a'=> 'two',
+	'b'=> 'four',
+	'c'=> 'six'
+}
+
+run.delete_if {|letter, word| letter >= 'b'}
+# => {"a"=>"two"}
+p run
+# {"a"=>"two"}
+# => {"a"=>"two"}
+
+#2. A method that filters a data structure for only items that do satisfy a certain condition
+#ARRAY
+numbers = [-2, -1, 0, 1, 2, 3, 4, 5]
+numbers.keep_if {|num| num < 3}
+# => [-2, -1, 0, 1, 2]
+p numbers
+# [-2, -1, 0, 1, 2]
+# => [-2, -1, 0, 1, 2]
+
+#HASH
+zoo = {
+	'sealions' => 'fishes',
+	'giraffes' => 'grass',
+	'rabbits' => 'carrots',
+	'panda' => 'bamboo'
+}
+
+zoo.delete('rabbits')
+# => "carrots"
+p zoo
+# {"sealions"=>"fishes", "giraffes"=>"grass", "panda"=>"bamboo"}
+# => {"sealions"=>"fishes", "giraffes"=>"grass", "panda"=>"bamboo"}
+
+#3. A different method that filters a data structure for only items satisfying a certain condition
+#ARRAY
+[29304, 9231, 120395, 9386, 19353].select {|digit| digit.odd?}
+# => [9231, 120395, 19353]
+
+#HASH
+difficult_numbers = {
+	pie_ish: 3,
+	pound: 16,
+	cup: 8,
+	tablespoon: 5
+}
+
+difficult_numbers.select {|constant, num| num.odd?}
+# => {:pie_ish=>3, :tablespoon=>5}
+p difficult_numbers
+# {:pie_ish=>3, :pound=>16, :cup=>8, :tablespoon=>5}
+# => {:pie_ish=>3, :pound=>16, :cup=>8, :tablespoon=>5}
+
+#4. A method that will remove items from a data structure until the 
+# condition in the block evaluates to false, then stops (you may not 
+# find a perfectly working option for the hash, and that's okay).
+
+nums = [23, 568, 35, 890, 64 ,9]
+nums.drop_while {|num| num < 600}
+# => [890, 64, 9]
+p nums
+# [23, 568, 35, 890, 64, 9]
+# => [23, 568, 35, 890, 64, 9]
 
