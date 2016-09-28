098143c0ea6eb563190bf128d0e755c9dd81452d
April Tang <april.tang09@gmail.com>
2016-09-28 10:45:08 -0700

    Call module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,11 @@
 module Shout
+	def self.yell_angrily(words)
+		words + "!!!" + " :("
+	end
+	def self.yelling_happily(words)
+		words + "yayayayayay!! Hip Hip Hoorray!"
+	end
 end
+
+p Shout.yell_angrily("bleeep")
+p Shout.yelling_happily("Hip Hip")
