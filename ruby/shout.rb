ff5dab5558397cd7b663a374901c618af044594a
April Tang <april.tang09@gmail.com>
2016-09-28 11:00:51 -0700

    Add mixin module

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,31 @@
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
-	end
-	def self.yelling_happily(words)
-		words + "yayayayayay!! Hip Hip Hoorray!"
+	def yell_angrily(words)
+		puts "#{words} !!!"
 	end
 end
 
-p Shout.yell_angrily("bleeep")
-p Shout.yelling_happily("Hip Hip")
+class Mom
+	include Shout
+end
+
+class Grandma
+	include Shout
+end
+
+mom = Mom.new
+mom.yell_angrily("bleeep")
+
+grandma = Grandma.new
+grandma.yell_angrily("HUMPH")
+
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+# 	def self.yelling_happily(words)
+# 		words + "yayayayayay!! Hip Hip Hoorray!"
+# 	end
+# end
+
+# p Shout.yell_angrily("bleeep")
+# p Shout.yelling_happily("Hip Hip")
