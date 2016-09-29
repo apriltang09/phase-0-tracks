c86baaccda486adec93da70588d6d2ddd6ab0165
April Tang <april.tang09@gmail.com>
2016-09-28 17:32:29 -0700

    Create class

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,22 +1,2 @@
 class TodoList
-	def initialize(get_items)
-		@get_items = get_items
-	end
-
-	def get_items
-		@get_items
-	end
-
-	def add_item(item)
-		@get_items << item
-	end
-
-	def delete_item(item)
-		@get_items.delete(item)
-	end
-
-	def get_item(num)
-		@get_items.fetch(num)
-	end
 end
-
