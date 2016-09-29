c676e5d27129a74c75495a4e9077e87731a2e067
April Tang <april.tang09@gmail.com>
2016-09-28 17:35:00 -0700

    Create add_item method to add item to array

--- ruby/list/todo_list.rb
+++ ruby/list/todo_list.rb
@@ -1,9 +1,12 @@
 class TodoList
-
 	def initialize(list_items)
 		@list_arr = list_items
 	end
 	def get_items
 		@list_arr
 	end
+	def add_item(item_to_add)
+		@list_arr << item_to_add
+	end
+
 end
