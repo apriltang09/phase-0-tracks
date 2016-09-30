1f3e240a87bfda477bb8ccd8b14ff8db22f514c2
April Tang <april.tang09@gmail.com>
2016-09-30 14:05:21 -0700

    Add methods to game class

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,2 +1,48 @@
 class Game
+	
+	attr_reader :guess_count
+	
+	def initialize(secret_word)
+		@secret_word = secret_word
+		@guess_count = @secret_word.length
+		@array = []
+	end
+	
+	def first_guess
+		@array = Array.new(@secret_word.length, '_')
+		p @array.join(' ')
+	end
+	
+	def guess_right(guess)
+
+		if !@secret_word.include? guess
+			p @array.join(' ')
+			@guess_count = @guess_count - 1
+			return 
+		end
+
+		i = 0
+		while i < @secret_word.length
+			if guess == @array[i]
+				p "You already guessed that!"
+				p @array.join(' ')
+				@guess_count
+			elsif guess == @secret_word[i]
+			@array[i] = @secret_word[i]
+				 	p @array.join(' ')
+					@guess_count = @guess_count - 1
+		end
+		@array
+		i+= 1
+		end
+	end
+	
+	def message
+		@array = @array.join
+		if @array == @secret_word
+			p "Winner!"
+		elsif @array != @secret_word
+			p "Nope!"
+		end
+	end
 end
