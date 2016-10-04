076f84afad83c200d3ac3fd82b8b48fd1c459274
April Tang <april.tang09@gmail.com>
2016-10-04 12:43:45 -0700

    Create an object using two arrays with for loop

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,4 +1,12 @@
-colors = ["blue", "red", "pink", "purple"]
-names = ["Ed", "Howard", "Bob", "Janet"]
-colors.push("green")
-names.push("Martha")
+var horses = {}
+var	i
+	colors = ["blue", "red", "pink", "purple"]
+	names = ["Ed", "Howard", "Bob", "Janet"]
+	colors.push("green")
+	names.push("Martha")
+
+for (i = 0; i < names.length; i++) {
+   	horses[names[i]] = colors[i];
+}
+
+console.log (horses)
