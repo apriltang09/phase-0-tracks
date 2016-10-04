64346e24fbda74c1dd09e76ac9adeaf208b9cd32
April Tang <april.tang09@gmail.com>
2016-10-04 12:45:23 -0700

    Add constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -10,3 +10,13 @@ for (i = 0; i < names.length; i++) {
 }
 
 console.log (horses)
+
+function Car(year, color, isAutomatic){
+	
+	console.log ("Car types:", this);
+	this.year = year;
+	this.color = color;
+	this.isAutomatic = isAutomatic;
+	this.sound = function() { console.log ("Vroom!"); };
+
+}
