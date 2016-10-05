83130e188100db0316e2c7f6c8289c0ef6d8df1a
April Tang <april.tang09@gmail.com>
2016-10-04 23:19:43 -0700

    Add function and store result of function in variable

--- js/explore.js
+++ js/explore.js
@@ -1,3 +1,16 @@
 // set a variable that holds an empty string
 // start at the end of the string
 // loop through each letter and add it to the variable that holds the string
+
+function reverse(string){
+	
+	var word = "";
+
+	for(var i = string.length - 1; i >= 0; i--){
+		word += string[i];
+	}
+	return word;
+}
+
+var greeting = reverse("hello");
+console.log(greeting)
