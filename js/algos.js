006b26734bd3635aae511baa428ddf977345c87f
April Tang <april.tang09@gmail.com>
2016-10-04 23:45:40 -0700

    Add algos function that finds longest word in array

--- js/algos.js
+++ js/algos.js
@@ -4,3 +4,17 @@
 // compare each string in the array to the current string length
 // if current array string is longer than the current longest string
 //assign the length of the array string as the longest string
+
+function algos(array){	
+	var longest = 0;
+	var word;
+	for (var i = 0; i < array.length; i++){	
+		if(array[i].length > longest){
+			longest = array[i].length;
+			word = array[i];
+		}
+	}	
+	return word;
+}
+
+algos(["long phrase","longest phrase","longer phrase"]);
