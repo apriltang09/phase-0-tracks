62ad500d16476999cf4948859f043a48b4b223bd
April Tang <april.tang09@gmail.com>
2016-10-16 17:10:57 -0700

    Add databasea and two tables

--- databases/tvhelp/tv.rb
+++ databases/tvhelp/tv.rb
@@ -1,1 +1,21 @@
 require 'sqlite3'
+
+db = SQLite3::Database.new("tvtracker.db")
+
+create_channels_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS channels(
+	id INTEGER PRIMARY KEY,
+	name VARCHAR(255)
+	)
+SQL
+
+create_tvshows_cmd = <<-SQL
+	CREATE TABLE IF NOT EXISTS tvshows(
+	id INTEGER PRIMARY KEY,
+	show VARCHAR(255),
+	day VARCHAR(255),
+	time INT,
+	channel_id INT,
+	FOREIGN KEY (channel_id) REFERENCES channels(id)
+	)
+SQL
